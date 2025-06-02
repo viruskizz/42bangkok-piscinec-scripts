4823ddd4d8ac1adf71150e8069a6f73e9760df85
Thitiwut Somsa <tsomsa@42bangkok.com>
2025-06-02 14:55:16 +0700

    fin: remote tester

--- rush01/tester_remote.sh
+++ rush01/tester_remote.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-URL="https://raw.githubusercontent.com/viruskizz/42bangkok-piscinec-scripts/refs/heads/main/rush01"
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+URL="https://raw.githubusercontent.com/viruskizz/42bangkok-piscinec-scripts/refs/heads/main/rush01/tester/"
 FILES=("main.py" "test.py" "validate_views.py" "generator.py" "run.sh")
 PWD=$(dirname "$0")
 DIR=rush01_tester
@@ -11,3 +14,6 @@ fi
 for file in "${FILES[@]}"; do
     curl -sL "${URL}/${file}" -o "${DIR}/${file}"
 done
+
+echo -e $YELLOW"Files downloaded to ${PWD}/${DIR}"$NC
+echo "Run the tests with:  ./${DIR}/run.sh"
